1beb94bc7be5e82abc51accfb04341565198acb0
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2022-06-17 16:39:25 -0300

    Working with vars, locals and envs

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # terraform-101
+
+* Running the commands using DEV (default)
+```
+terraform plan 
+```
+* Running the commands using PROD
+```
+terraform plan -var-file="prod.tfvars"
+```
--- main.tf
+++ main.tf
@@ -1,28 +1,30 @@
 terraform {
   required_version = "1.2.3"
   required_providers {
-    aws = { 
-        sousource = "hashicorp/aws"
-        version = "4.19.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.19.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  profile = "terraform-101"
+  region  = var.aws_region
+  profile = var.aws_profile
 }
 
 resource "aws_s3_bucket" "terraform-test-bucket" {
-  bucket = "my-tf-test-bucket-234232387394876837648"  
-
-  tags = {
-    Name        = "Terraform Test Bucket"
-    Environment = "DEV"
-  }
+  bucket = local.terraform_test_bucket_name
+  tags   = local.terraform_test_bucket_tags
 }
 
 resource "aws_s3_bucket_acl" "terraform-test-bucket-acl" {
   bucket = aws_s3_bucket.terraform-test-bucket.id
   acl    = "private"
+}
+
+resource "aws_instance" "ec2-test" {
+  ami           = var.ec2_test_ami
+  instance_type = var.ec2_test_instance_type
+  tags          = local.terraform_ec2_test_tags
 }
